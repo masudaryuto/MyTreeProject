31a1930f3174c58e93d3668f9236a6035d021185
masudaryuto <masuda200217@icloud.com>
2022-09-17 17:43:51 +0900

    add controller

--- Java_Program/forest/Controller.java
+++ Java_Program/forest/Controller.java
@@ -11,6 +11,8 @@ import java.awt.Font;
 import javax.swing.JLabel;
 import java.awt.Color;
 import javax.swing.border.LineBorder;
+import java.awt.Component;
+import java.awt.Cursor;
 
 /*
  * 
@@ -23,11 +25,16 @@ public class Controller extends Object implements MouseListener, MouseMotionList
 
 	private Boolean clickFlag;
 
+	private Point previous;
+	private Point current;
+
 	public Controller(Model aModel) {
 		super();
 		this.model = aModel;
 		this.view = null;
 		this.clickFlag = false;
+		this.previous = null;
+		this.current = null;
 
 		return;
 	}
@@ -36,42 +43,48 @@ public class Controller extends Object implements MouseListener, MouseMotionList
 		return;
 	}
 
-	public void moveTree() {
+	public void moveTree() { return; }
 
-
-
-		return;
-	}
-
-	public void mouseClicked(MouseEvent e){ 
-
-		this.clickFlag ^= true;
-
-
-		return; 
-	}
+	public void mouseClicked(MouseEvent e){ return; }
 	public void mouseEntered(MouseEvent e){ return; }
 	public void mouseExited(MouseEvent e){ return; }
-	public void mousePressed(MouseEvent e){ return; }
+	public void mousePressed(MouseEvent e){ 
+		System.out.println("mouse Click");
+	
+		this.clickFlag ^= true;
+	
+		Cursor aCursor = Cursor.getDefaultCursor();
+		Component aComponent = (Component)e.getSource();
+		aComponent.setCursor(aCursor);
+	
+		this.current = e.getPoint();
+		this.previous = this.current;
+		return; 
+	}
+
 	public void mouseReleased(MouseEvent e){ return; }
 
 	
-	public void mouseMoved(MouseEvent e){
-		// System.out.println("mouseMoved");
-		
-		return;
-	}
+	public void mouseMoved(MouseEvent e){ return; }
 
 	public void mouseDragged(MouseEvent e){
 		System.out.println("mouseDragged");
-		Point aClickPoint = e.getPoint();
+		this.current = e.getPoint();
+		int x = this.current.x - this.previous.x;
+		int y = this.current.y - this.previous.y;
 		ArrayList<Point> aPointList = this.model.getPosition();
-		
 		HashMap<Integer, Integer> branchesMap = this.model.getBranchMap();
-		
 		ArrayList<String> nodes = this.model.getNodes();
 
+		//カーソル
+		Cursor aCursor = Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR);
+		Component aComponent = (Component)e.getSource();
+		aComponent.setCursor(aCursor);
+		
+		
+		
 
+		System.out.printf("point.x:%d, point.y:%d\n", this.current.x, this.current.y);
 		//サイズの自動化？
 		
 		for(int address=1; address<nodes.size(); address++){
@@ -82,30 +95,41 @@ public class Controller extends Object implements MouseListener, MouseMotionList
 			aLabel.setBounds(aPoint.x, aPoint.y , aDimension.width, aDimension.height);
 			aLabel.setBorder(new LineBorder(Color.BLACK, 1, false));
 			aLabel.setHorizontalAlignment(JLabel.CENTER);
+
 			Point aNodePoint = aPointList.get(address);
-			aLabel.setSize(aLabel.getPreferredSize());
+			// aLabel.setSize(aLabel.getPreferredSize());
 
-			// System.out.println(nodes.get(address));
-			if(nodes.get(address).equals(" Magnitude")){
+			
 
-				// System.out.printf("x:%d, click.x:%d, aNodePoint.x + aLabel.getWidth():%d\n", aNodePoint.x, aClickPoint.x, aNodePoint.x + aLabel.getWidth());
-				// System.out.printf("y:%d, click.y:%d, aNodePoint.y + aLabel.getHeight():%d\n", aNodePoint.y, aClickPoint.y, aNodePoint.y + aLabel.getHeight());
-			}
 			// System.out.println();
 			//x
-			if(aNodePoint.x < aClickPoint.x && aClickPoint.x < aNodePoint.x + aLabel.getWidth()){
-				//y
-				if(aNodePoint.y + (2*15) < aClickPoint.y && aClickPoint.y < aNodePoint.y + aLabel.getHeight() + (2*15)){
-					System.out.println(nodes.get(address));
+			// if(aNodePoint.x < this.current.x && this.current.x < aNodePoint.x + aLabel.getWidth()){
+			// 	//y
+			// 	if(aNodePoint.y < this.current.y && this.current.y < aNodePoint.y + aLabel.getHeight()){
+			// 		System.out.println(nodes.get(address));
+			// 		System.out.printf("x：%d < %d < %d\n", aNodePoint.x, this.current.x, aNodePoint.x + aLabel.getWidth());
+			// 		System.out.printf("y：%d < %d < %d\n", aNodePoint.y + (2*15), this.current.y, aNodePoint.y + aLabel.getHeight() + (2*15));
+					
+			// 		this.model.UpdatePosition(address, aNodePoint.x + x, aNodePoint.y + y);
 
-					this.model.UpdatePosition(address, aClickPoint.x, aClickPoint.y - (2*15));
+			// 		this.view.moveLabel(address);
 
-					this.view.moveLabel(address);
-
-					break;
-				}
-			}
+			// 		break;
+			// 	}
+			// }
+			
+			this.model.UpdatePosition(address, aNodePoint.x + x, aNodePoint.y + y);
+			
+			this.view.moveLabel(address);
+		
+			
+			
 		}
+
+		
+		System.out.println(x);
+		System.out.println(y);
+		this.previous = this.current;
 
 
 		return;
